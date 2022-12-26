0ff0719f5340e2175de1d8656a757fb8b8da6f64
Zekao <Busterk182@gmail.com>
2022-12-26 13:50:26 +0100

    problem in worker

--- p1/scripts/k3s_master.sh
+++ p1/scripts/k3s_master.sh
@@ -5,11 +5,14 @@ echo "[INFO]  Installing k3s on server node (ip: $1)"
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san emaugaleS --node-ip $1  --bind-address=$1 --advertise-address=$1 --token-file=/vagrant/$2 "
 
 echo "[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
-# apk add curl
 
-# curl -sfL https://get.k3s.io |  sh -
+cat /vagrant/$2
+
+apk add curl
+
+curl -sfL https://get.k3s.io |  sh -
 
 echo "[INFO]  Successfully installed k3s on server node"
 
-# echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
+echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
 
--- p1/scripts/k3s_slave.sh
+++ p1/scripts/k3s_slave.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 export NAME="emaugaleSW"
+
 echo "[INFO]  Installing k3s on server worker node (ip: $2)"
 
-export INSTALL_K3S_EXEC="agent --server https://$1:6643 --token-file=/vagrant/$3 --node-ip $2 --tls-san $NAME"
+
+export k3s_token=$(cat /vagrant/$3)
+
+echo " [INFO] TOKEN ON WORKER SIDE: $k3s_token"
+
+export INSTALL_K3S_EXEC="agent --node-ip $2 --tls-san $NAME"
 
 apk add curl
-curl -sfL https://get.k3s.io |  sh -
 
-# sudo apt-get update
-# sudo apt-get install net-tools -y
+curl -sfL https://get.k3s.io | K3S_URL=https://$1:6443 K3S_TOKEN=${k3s_token} sh -
+
 echo "alias k='kubectl'" >> /etc/profile.d/00-aliases.sh # way to add alias on all users
