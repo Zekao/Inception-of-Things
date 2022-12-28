3d9b7fb88c5a8b83863cf0d68cad91ceca70ac86
Ubuntu <ubuntu@ns3008321.ip-151-80-45.eu>
2022-12-28 15:54:50 +0000

    p2 done

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -3,4 +3,4 @@ apk add curl
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip 192.168.56.110  --bind-address=192.168.56.110 --advertise-address=192.168.56.110 "
 curl -sfL https://get.k3s.io | sh -
 sleep 10
-kubectl apply -f /vagrant/scripts/deploy.yml
+kubectl apply -f /vagrant/scripts/deploy.yaml
