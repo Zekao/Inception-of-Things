1520a098d3c74161ec5f6e624eea37828f8a9987
Zekao <Busterk182@gmail.com>
2022-12-28 20:05:01 +0100

    finished p3

--- p3/scripts/argocd_install.sh
+++ p3/scripts/argocd_install.sh
@@ -12,9 +12,15 @@ echo "[INFO]  Successfully created argocd namespace"
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "[INFO]  Recovering argocd password and doing port forwarding"
+echo "[INFO]  Recovering argocd password"
 
-echo -n "ArgoCD password:"	
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+while [[ $(kubectl get pods -n argocd -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True True True True True" ]]; do echo "[INFO]  Waiting all pods to be ready" && sleep 5; done
+
+echo -n "ArgoCD password: "
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+echo "[INFO]  Doing port forwarding"
+sleep 5
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
--- p3/scripts/k3d_install.sh
+++ p3/scripts/k3d_install.sh
@@ -6,6 +6,6 @@ echo "[INFO]  Successfully installed docker on server node"
 
 echo "[INFO]  Installing k3d on server node (ip: $1)"
 
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh |  bash
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 
 echo "[INFO]  Successfully installed k3d on server node"
