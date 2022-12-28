c2e810da40abd770459897c4fce4955a11c8819b
Lucas SEHAIRI <lusehair@e1r9p5.clusters.42paris.fr>
2022-12-28 12:23:04 +0100

    p2 in on good way, need to fix yml file

---
+++ p2/scripts/server.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apk add curl
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip 192.168.56.110  --bind-address=192.168.56.110 --advertise-address=192.168.56.110 "
+curl -sfL https://get.k3s.io | sh -
+sleep 10
+kubectl apply -f /vagrant/scripts/deploy.yml
