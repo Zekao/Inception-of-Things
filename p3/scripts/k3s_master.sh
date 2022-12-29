3ce2cae7160f9f9f9c31e9275d975d6f3b065c06
Zekao <Busterk182@gmail.com>
2022-12-29 01:47:47 +0100

    finished p3

--- p3/scripts/k3s_master.sh
+++ p3/scripts/k3s_master.sh
@@ -2,7 +2,7 @@
 
 echo "[INFO]  Installing k3s on server node (ip: $1)"
 
-export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname)"
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --tls-san $(hostname) --node-ip $1  --bind-address=$1 --advertise-address=$1 "
 
 echo "[INFO]  ARGUMENT PASSED TO INSTALL_K3S_EXEC: $INSTALL_K3S_EXEC"
 
